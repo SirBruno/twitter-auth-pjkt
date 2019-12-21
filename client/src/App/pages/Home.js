77df5eff4924d3b78c99a1a99e0c3844cb5fa6e4
Bruno <bruno.webjr@gmail.com>
2019-12-21 16:59:19 -0300

    ...

---
+++ client/src/App/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class Home extends Component {
+  render() {
+    return (
+    <div className="App">
+      <h1>Project Home</h1>
+        <a href="http://localhost:5000/auth/twitter">adasdasd</a>
+    </div>
+    );
+  }
+}
+export default Home;
